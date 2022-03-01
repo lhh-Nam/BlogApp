41a391fd315d5a9ea024687dceefe30c34327506
lhh-Nam <lehoanghuynam0401@gmail.com>
2022-03-01 10:43:06 +0700

    add register

--- server/index.ts
+++ server/index.ts
@@ -5,19 +5,18 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
+import routes from './routers'
 
 // Middleware
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(morgan('dev'))
 app.use(cookieParser())
 
 // Routes
-app.get('/', (req, res) => {
-    res.json({msg: 'Heluuuuu'})
-})
+app.use('/api', routes.authRouter)
 
 // Database
 import './config/database'
@@ -25,5 +24,5 @@ import './config/database'
 // server listenning
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+  console.log('Server is running on port', PORT)
 })
---
+++ server/models/userModel.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add your name'],
+      trim: true,
+      maxLength: [20, 'Your name is up to 20 chars long'],
+    },
+    account: {
+      type: String,
+      required: [true, 'Please add your email or phone'],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add your password'],
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      default: 'https://res.cloudinary.com/jayseak/image/upload/v1619633050/sample.jpg',
+    },
+    role: {
+      type: String,
+      default: 'user',
+    },
+    type: {
+      type: String,
+      default: 'normal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+export default mongoose.model('User', userSchema)
