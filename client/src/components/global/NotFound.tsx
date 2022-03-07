674a6225d2376e6ccec70fbbd6d64f48e492a531
lhh-Nam <lehoanghuynam0401@gmail.com>
2022-03-07 21:30:29 +0700

    create client router

---
+++ client/src/components/global/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div  style={{minHeight: 'calc(100vh - 70px)', position:'relative'}}>
+      <h2 style={{position:'absolute', top:'50%', left:'50%', transform:'translate(-50%,-50%)'}}>404 | NotFound</h2>
+    </div>
+  )
+}
+
+export default NotFound
